164039d3dfa73ea4e6b62fee3913821ca0c86bd7
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-08-18 11:44:39 -0400

    Post feature complete

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parses incoming data into JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { animals } = require("./data/animals");
 
 function filterByQuery(query, animalsArray) {
@@ -53,7 +59,37 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+
+  animalsArray.push(animal);
+
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
+  //allows users to make a GET request using a query parameter
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -62,11 +98,26 @@ app.get("/api/animals", (req, res) => {
 });
 
 app.get("/api/animals/:id", (req, res) => {
+  //allows users to make a GET request using ID to retrieve a certain object
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
   } else {
     res.sendStatus(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  //sets id based on what the index of the array should be (the length property is always one number ahead if the last index of an array hence why this works)
+  //works as long as no data is removed from array
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
   }
 });
 
